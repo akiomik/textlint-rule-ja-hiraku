fe32262504ce299b60c21e574d67542c564af2d8
Akiomi Kamakura <akiomik@gmail.com>
2023-12-05 01:47:59 +0900

    Use 2sp

--- test/index-test.ts
+++ test/index-test.ts
@@ -4,43 +4,42 @@ import rule from "../src/index";
 const tester = new TextLintTester();
 // ruleName, rule, { valid, invalid }
 tester.run("rule", rule, {
-    valid: [
-        // no problem
-        "text",
+  valid: [
+    // no problem
+    "text",
+    {
+      text: "It is bugs, but it should be ignored",
+      options: {
+        allows: ["it should be ignored"]
+      }
+    }
+  ],
+  invalid: [
+    // single match
+    {
+      text: "It is bugs.",
+      errors: [
         {
-            text: "It is bugs, but it should be ignored",
-            options: {
-                allows: ["it should be ignored"]
-            }
+          message: "Found bugs.",
+          range: [6, 10]
         }
-    ],
-    invalid: [
-        // single match
-        {
-            text: "It is bugs.",
-            errors: [
-                {
-                    message: "Found bugs.",
-                    range: [6, 10]
-                }
-            ]
-        },
-        // multiple match
-        {
-            text: `It has many bugs.
+      ]
+    },
+    // multiple match
+    {
+      text: `It has many bugs.
 
 One more bugs`,
-            errors: [
-                {
-                    message: "Found bugs.",
-                    range: [12, 16]
-                },
-                {
-                    message: "Found bugs.",
-                    range: [28, 32]
-                }
-            ]
+      errors: [
+        {
+          message: "Found bugs.",
+          range: [12, 16]
         },
-
-    ]
+        {
+          message: "Found bugs.",
+          range: [28, 32]
+        }
+      ]
+    },
+  ]
 });
