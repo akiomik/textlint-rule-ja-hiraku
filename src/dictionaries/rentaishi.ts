5d09dfb954119bf26a1dd2f31e371fd2d389edd2
Akiomi Kamakura <akiomik@gmail.com>
2023-12-15 07:40:06 +0900

    Add 連体詞

--- src/dictionaries/rentaishi.ts
+++ src/dictionaries/rentaishi.ts
@@ -11,4 +11,52 @@ export const rentaishi = [
       },
     ],
   },
+  {
+    message: '平仮名にひらいたほうが読みやすい漢字を使用しています。',
+    expected: 'いろんな',
+    tokens: [
+      {
+        surface_form: '色んな',
+        basic_form: '色んな',
+        pos: '連体詞',
+        reading: 'イロンナ',
+      },
+    ],
+  },
+  // {
+  //   message: '平仮名にひらいたほうが読みやすい漢字を使用しています。',
+  //   expected: 'いわゆる',
+  //   tokens: [
+  //     {
+  //       surface_form: '所謂',
+  //       basic_form: '所謂',
+  //       pos: '連体詞',
+  //       reading: 'イワユル',
+  //     },
+  //   ],
+  // },
+  {
+    message: '平仮名にひらいたほうが読みやすい漢字を使用しています。',
+    expected: 'このまえ',
+    tokens: [
+      {
+        surface_form: '此の前',
+        basic_form: '此の前',
+        pos: '名詞',
+        reading: 'コノマエ',
+      },
+    ],
+  },
+  {
+    message: '平仮名にひらいたほうが読みやすい漢字を使用しています。',
+    expected: 'その',
+    tokens: [
+      {
+        surface_form: '其の',
+        basic_form: '其の',
+        pos: '連体詞',
+        reading: 'ソノ',
+      },
+    ],
+  },
 ];
