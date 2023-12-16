1e8cbb52f48af0e7ad9dbc236ef05692753cc221
Akiomi Kamakura <akiomik@gmail.com>
2023-12-17 03:41:09 +0900

    Add options support

---
+++ src/loader.ts
@@ -0,0 +1,90 @@
+import type { ExpectedDictionary } from 'morpheme-match-all';
+import type { ExpectedTokenWithCapture } from 'morpheme-match-textlint';
+
+import { fukujoshi } from './dictionaries/fukujoshi';
+import { fukushi } from './dictionaries/fukushi';
+import { hojodoushi } from './dictionaries/hojodoushi';
+import { keishikimeishi } from './dictionaries/keishikimeishi';
+import { otherDoushi } from './dictionaries/other-doushi';
+import { otherJodoushi } from './dictionaries/other-jodoushi';
+import { otherKandoushi } from './dictionaries/other-kandoushi';
+import { otherKeiyoushi } from './dictionaries/other-keiyoushi';
+import { otherMeishi } from './dictionaries/other-meishi';
+import { rentaishi } from './dictionaries/rentaishi';
+import { setsuzokushi } from './dictionaries/setsuzokushi';
+import type { DictOpts } from './opts';
+
+const defaultOpts: DictOpts = {
+  fukushi: true,
+  fukujoshi: true,
+  hojodoushi: true,
+  keishikimeishi: true,
+  'other-doushi': true,
+  'other-jodoushi': true,
+  'other-kandoushi': true,
+  'other-keiyoushi': true,
+  'other-meishi': true,
+  rentaishi: true,
+  setsuzokushi: true,
+};
+
+export class DictionaryLoader {
+  private options: DictOpts;
+
+  constructor(options: Partial<DictOpts>) {
+    this.options = { ...defaultOpts, ...options };
+  }
+
+  load<
+    T extends ExpectedTokenWithCapture,
+    Dictionary extends ExpectedDictionary<T>,
+  >(): Dictionary[] {
+    let dict = [] as Dictionary[];
+
+    if (this.options.fukujoshi) {
+      dict = [...(fukujoshi as Dictionary[])];
+    }
+
+    if (this.options.fukushi) {
+      dict = [...dict, ...(fukushi as Dictionary[])];
+    }
+
+    if (this.options.hojodoushi) {
+      dict = [...dict, ...(hojodoushi as Dictionary[])];
+    }
+
+    if (this.options.keishikimeishi) {
+      dict = [...dict, ...(keishikimeishi as Dictionary[])];
+    }
+
+    if (this.options['other-doushi']) {
+      dict = [...dict, ...(otherDoushi as Dictionary[])];
+    }
+
+    if (this.options['other-jodoushi']) {
+      dict = [...dict, ...(otherJodoushi as Dictionary[])];
+    }
+
+    if (this.options['other-kandoushi']) {
+      dict = [...dict, ...(otherKandoushi as Dictionary[])];
+    }
+
+    if (this.options['other-keiyoushi']) {
+      dict = [...dict, ...(otherKeiyoushi as Dictionary[])];
+    }
+
+    if (this.options['other-meishi']) {
+      dict = [...dict, ...(otherMeishi as Dictionary[])];
+    }
+
+    if (this.options.rentaishi) {
+      dict = [...dict, ...(rentaishi as Dictionary[])];
+    }
+
+    if (this.options.setsuzokushi) {
+      dict = [...dict, ...(setsuzokushi as Dictionary[])];
+    }
+
+    return dict;
+  }
+}
--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import TextLintTester from 'textlint-tester';
 import rule from '../src/index';
 
 const tester = new TextLintTester();
-tester.run('rule', rule, {
+tester.run('index', rule, {
   valid: [
     'あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。',
   ],
